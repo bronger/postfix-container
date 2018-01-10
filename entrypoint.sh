16e1ff81bee58aab36ecb645238bec3bb89a8d97
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-10 08:20:55 +0100

    Removed command only used for debugging purposes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ set -e
 postconf -e "relayhost=$HOST:$PORT"
 echo "$HOST $USER:$PASSWORD" >> /etc/postfix/relay_passwd
 service postfix start
-ls -l /var/log
 supervisord -c /etc/supervisor/supervisord.conf
