cad1ea185b3429f59f01aa812560824fa14525ea
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-10 08:21:24 +0100

    Fixed logging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 postconf -e "relayhost=$HOST:$PORT"
 echo "$HOST $USER:$PASSWORD" >> /etc/postfix/relay_passwd
+/usr/sbin/rsyslogd
 service postfix start
 supervisord -c /etc/supervisor/supervisord.conf
