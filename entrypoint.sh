5a2c60b18559ac413dfc138230234c1abd8b4e24
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-10 08:22:02 +0100

    Fixed storage of SASL credentials.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 postconf -e "relayhost=$HOST:$PORT"
 echo "$HOST $USER:$PASSWORD" >> /etc/postfix/relay_passwd
+postmap hash:/etc/postfix/relay_passwd
 /usr/sbin/rsyslogd
 service postfix start
 supervisord -c /etc/supervisor/supervisord.conf
