14d74f320d65d1bba05687491067e3fd8cf2f948
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-18 14:55:09 +0100

    Renamed environment variables to avoid name clashes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 /configure_sigh.py "$SIGH_ROOT"
-postconf -e "relayhost=$HOST:$PORT"
-echo "$HOST $USER:$PASSWORD" >> /etc/postfix/relay_passwd
+postconf -e "relayhost=$RELAY_HOST:$RELAY_PORT"
+echo "$RELAY_HOST $RELAY_USER:$RELAY_PASSWORD" >> /etc/postfix/relay_passwd
 postmap hash:/etc/postfix/relay_passwd
 /usr/sbin/rsyslogd
 service postfix start
