c15de1a1f946ae3604ce5fe3e3c8eb7f87122e8c
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-20 15:37:36 +0100

    Improved source code layout.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ postconf -e "relayhost=$RELAY_HOST:$RELAY_PORT"
 echo "$RELAY_HOST $RELAY_USER:$RELAY_PASSWORD" >> /etc/postfix/relay_passwd
 postmap hash:/etc/postfix/relay_passwd
 /usr/sbin/rsyslogd
+
 supervisord -c /etc/supervisor/supervisord.conf &
 supervisord_pid=$!
 wait $supervisord_pid
