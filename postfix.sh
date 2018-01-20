7770d7427d383350f94db9cfe0a2af6f8eb5ed77
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-20 15:26:18 +0100

    Enabled supervisord to detect an enexpected termination of Postfix.

--- postfix.sh
+++ postfix.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 # Taken from <https://gist.github.com/chrisnew/b0c1b8d310fc5ceaeac4>.
 
-trap "postfix stop" SIGTERM
+trap "postfix stop; result=0" SIGTERM
 trap "postfix reload" SIGHUP
 
 postfix start
 sleep 3
 
 PID=`cat /var/spool/postfix/pid/master.pid`
+result=1
 while kill -0 $PID
 do
     sleep 1
 done
+exit $result
