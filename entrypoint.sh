f62f9b3ea1c35ea81216afe242ac5fb4ac5eda1d
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-20 20:06:23 +0100

    Made dealing with SIGTERM more robust.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,19 @@ set -e
 
 # The propagating of SIGTERM was taken from
 # <http://veithen.github.io/2014/11/16/sigterm-propagation.html>.
-trap "kill -s SIGTERM $supervisord_pid" TERM
+trap "kill -s SIGTERM $supervisord_pid; ITS_A_TRAP=true" TERM
 
 /configure_sigh.py "$SIGH_ROOT"
 postconf -e "relayhost=$RELAY_HOST:$RELAY_PORT"
 echo "$RELAY_HOST $RELAY_USER:$RELAY_PASSWORD" >> /etc/postfix/relay_passwd
 postmap hash:/etc/postfix/relay_passwd
 
+ITS_A_TRAP=false
 supervisord -c /etc/supervisor/supervisord.conf &
 supervisord_pid=$!
 wait $supervisord_pid
 result=$?
-if [ $result -le 128 ]
+if [ "$ITS_A_TRAP" == "false" ]
 then
    exit $result
 fi
