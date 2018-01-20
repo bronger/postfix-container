c204eaefd00570cb3236a67173044f31f4a08ac3
Torsten Bronger <bronger@physik.rwth-aachen.de>
2018-01-20 12:22:02 +0100

    Made entrypoint propagate a SIGTERM to supervisord.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -e
 
+# The propagating of SIGTERM was taken from
+# <http://veithen.github.io/2014/11/16/sigterm-propagation.html>.
+trap "kill -s SIGTERM $supervisord_pid" TERM
+
 /configure_sigh.py "$SIGH_ROOT"
 postconf -e "relayhost=$RELAY_HOST:$RELAY_PORT"
 echo "$RELAY_HOST $RELAY_USER:$RELAY_PASSWORD" >> /etc/postfix/relay_passwd
 postmap hash:/etc/postfix/relay_passwd
 /usr/sbin/rsyslogd
 service postfix start
-supervisord -c /etc/supervisor/supervisord.conf
+supervisord -c /etc/supervisor/supervisord.conf &
+supervisord_pid=$!
+wait $supervisord_pid
+trap - TERM
+wait $supervisord_pid
+exit $?
